fcf838a42a33d05cd564384dc163cb0759317c95
fanta-git <fantaji4957t@gmail.com>
2023-09-25 13:49:22 +0900

    fix: 初期アイコンを読み込めなかった

--- next.config.js
+++ next.config.js
@@ -4,9 +4,20 @@ const nextConfig = {
     remotePatterns: [
       {
         hostname: "nicovideo.cdn.nimg.jp",
+        port: "",
+        pathname: "/thumbnails/**"
       },
       {
+        protocol: "https",
         hostname: "*.cloudfront.net",
+        port: "",
+        pathname: "/avatar/**"
+      },
+      {
+        protocol: "https",
+        hostname: "kiite.jp",
+        port: "",
+        pathname: "/img/**",
       },
     ],
   },
